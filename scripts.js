c596cb8e7f670b022cbdbd983ef03e1c2a055cfa
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-08-31 08:23:09 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Sardaran");
+$("#policyNumber").text("DAI7248701");
+$("#startDate").text("01-Sep-2020");
+$("#endDate").text("31-Aug-2021");
+
+
+$("#type").text("Two Wheeler");
+$("#registrationNumber").text("PB08BV3882");
+$("#make").text("HONDA");
+$("#model").text("AVIATOR");
+$("#vehicalSubType").text("STD");
+$("#engineNo").text("JF21E9208900");
+$("#chassis").text("ME4JF213EB8186535");
